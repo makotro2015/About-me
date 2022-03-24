c5203cb59931e80891f6c18ae5a0d4f3475eaac4
makotro2015 <makotro2015@gmail.com>
2022-03-24 22:08:15 +0700

    Updates

---
+++ precache-manifest.bcd93dc166f2407aaac2d9ad92dc2ae0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9ee18a1f97da4a30d46638c51e2b9c",
+    "url": "/About-me/index.html"
+  },
+  {
+    "revision": "6308d93734079095e5a1",
+    "url": "/About-me/static/css/main.829e3c8a.chunk.css"
+  },
+  {
+    "revision": "b239d2bf21ba222ce057",
+    "url": "/About-me/static/js/2.5207678d.chunk.js"
+  },
+  {
+    "revision": "6308d93734079095e5a1",
+    "url": "/About-me/static/js/main.43d34110.chunk.js"
+  },
+  {
+    "revision": "27f5a7082b85990cb679",
+    "url": "/About-me/static/js/runtime~main.2ff819fe.js"
+  },
+  {
+    "revision": "d2bec4abc22b711d3f325a54c5c84276",
+    "url": "/About-me/static/media/FactorA-Bold.d2bec4ab.ttf"
+  },
+  {
+    "revision": "6b8fcd64be4a90b35eb8b5f9d180b830",
+    "url": "/About-me/static/media/FactorAMedium-Regular.6b8fcd64.ttf"
+  },
+  {
+    "revision": "c0be81e0185a52f6192d099ede876837",
+    "url": "/About-me/static/media/author.c0be81e0.JPG"
+  },
+  {
+    "revision": "b1d1a23511fa53633b9bc5dcf9b8688a",
+    "url": "/About-me/static/media/bg-image.b1d1a235.png"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/About-me/static/media/gilroy-bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "608ff54bc34640ddb03027a5f90f4217",
+    "url": "/About-me/static/media/gilroy-light.608ff54b.ttf"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/About-me/static/media/gilroy-medium.6444f14a.ttf"
+  }
+]);
